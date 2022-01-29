8bfae5413207c45128f27bb9f89b234fa945ced8
Kent <dkentpatrickd@gmail.com>
2022-01-29 16:55:32 +0800

    My Gcard initial layout.

--- useraccount/build/generated/source/buildConfig/debug/org/rmj/guanzongroup/useraccount/BuildConfig.java
+++ useraccount/build/generated/source/buildConfig/debug/org/rmj/guanzongroup/useraccount/BuildConfig.java
@@ -5,6 +5,6 @@ package org.rmj.guanzongroup.useraccount;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "org.rmj.guanzongroup.authlibrary";
+  public static final String LIBRARY_PACKAGE_NAME = "org.rmj.guanzongroup.useraccount";
   public static final String BUILD_TYPE = "debug";
 }
