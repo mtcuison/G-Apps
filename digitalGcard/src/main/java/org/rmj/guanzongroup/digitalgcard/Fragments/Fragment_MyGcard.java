b98dd4cb1c13c1e6961a2bdd02583f6e4d55cecc
Kent <dkentpatrickd@gmail.com>
2022-02-01 13:28:02 +0800

    Set separate activity appbar theme.

--- digitalGcard/src/main/java/org/rmj/guanzongroup/digitalgcard/Fragments/Fragment_MyGcard.java
+++ digitalGcard/src/main/java/org/rmj/guanzongroup/digitalgcard/Fragments/Fragment_MyGcard.java
@@ -2,6 +2,7 @@ package org.rmj.guanzongroup.digitalgcard.Fragments;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,14 +12,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.rmj.guanzongroup.appcore.GCardCore.GCardSystem;
+import org.rmj.guanzongroup.digitalgcard.Activity_ManageGcard;
 import org.rmj.guanzongroup.digitalgcard.R;
 import org.rmj.guanzongroup.digitalgcard.ViewModel.VMGCardSystem;
 
 public class Fragment_MyGcard extends Fragment {
 
     private VMGCardSystem mViewModel;
+    private TextView txtManage;
 
     public static Fragment_MyGcard newInstance() {
         return new Fragment_MyGcard();
@@ -38,10 +42,15 @@ public class Fragment_MyGcard extends Fragment {
         mViewModel = new ViewModelProvider(requireActivity()).get(VMGCardSystem.class);
         mViewModel.setInstance(GCardSystem.CoreFunctions.GCARD);
 
+        txtManage.setOnClickListener(v -> {
+            Intent loIntent = new Intent(getActivity(), Activity_ManageGcard.class);
+            startActivity(loIntent);
+        });
+
     }
 
     private void initViews(View v) {
-
+        txtManage = v.findViewById(R.id.lblManageGcard);
     }
 
 }
