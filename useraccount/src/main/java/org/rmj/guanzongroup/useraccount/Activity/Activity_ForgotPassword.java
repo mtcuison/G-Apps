487afbe21fc7497085c939326af29c373d3b6672
Kent <dkentpatrickd@gmail.com>
2022-02-04 11:33:21 +0800

    Forgot password layout.

---
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_ForgotPassword.java
@@ -0,0 +1,16 @@
+package org.rmj.guanzongroup.useraccount.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.rmj.guanzongroup.useraccount.R;
+
+public class Activity_ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
