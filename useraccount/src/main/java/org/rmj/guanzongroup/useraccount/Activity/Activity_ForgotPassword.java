2fc7ae29d4c887f68b5810cd9d0c9d19fb257927
Kent <dkentpatrickd@gmail.com>
2022-02-04 15:31:16 +0800

    Create login layout and setup Forgot Password.

--- useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_ForgotPassword.java
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_ForgotPassword.java
@@ -1,8 +1,10 @@
 package org.rmj.guanzongroup.useraccount.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import org.rmj.guanzongroup.useraccount.R;
 
@@ -13,4 +15,18 @@ public class Activity_ForgotPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
 }
--- useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_Login.java
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_Login.java
@@ -4,8 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.rmj.guanzongroup.useraccount.R;
 
@@ -14,6 +23,9 @@ import java.util.Objects;
 public class Activity_Login extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private TextView lblUser, lblForgot, lblCreate;
+    private TextInputLayout tilEmail, tilMobile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,8 @@ public class Activity_Login extends AppCompatActivity {
 
         initViews();
         setUpToolbar();
+        setTabLayout();
+        setClickLinkListeners();
     }
 
     @Override
@@ -40,6 +54,15 @@ public class Activity_Login extends AppCompatActivity {
     // Initialize this first before anything else.
     private void initViews() {
         toolbar = findViewById(R.id.toolbar);
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Email"));
+        tabLayout.addTab(tabLayout.newTab().setText("Mobile"));
+
+        tilEmail = findViewById(R.id.til_email);
+        tilMobile = findViewById(R.id.til_mobile);
+        lblUser = findViewById(R.id.lblUser);
+        lblForgot = findViewById(R.id.lblForgotPassword);
+        lblCreate = findViewById(R.id.lblSignUp);
     }
 
     // Initialize initViews() before this method.
@@ -47,6 +70,48 @@ public class Activity_Login extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Log In");
+    }
+
+    private void setClickLinkListeners() {
+        lblForgot.setOnClickListener(v -> {
+            Intent loIntent = new Intent(Activity_Login.this, Activity_ForgotPassword.class);
+            startActivity(loIntent);
+        });
+
+        lblCreate.setOnClickListener(v -> {
+            Intent loIntent = new Intent(Activity_Login.this, Activity_SignUp.class);
+            startActivity(loIntent);
+        });
+    }
+
+    private void setTabLayout(){
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch(tab.getPosition()) {
+                    case 1:
+                        tilEmail.setVisibility(View.INVISIBLE);
+                        tilMobile.setVisibility(View.VISIBLE);
+                        lblUser.setText(R.string.lblMobileNumber);
+                        break;
+                    default:
+                        tilEmail.setVisibility(View.VISIBLE);
+                        tilMobile.setVisibility(View.INVISIBLE);
+                        lblUser.setText(R.string.lblEmail);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
 }
