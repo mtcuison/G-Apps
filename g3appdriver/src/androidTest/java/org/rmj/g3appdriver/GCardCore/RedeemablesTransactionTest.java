61bbc59717895d39e2beedfe9348e44609da5c5e
mikegarcia8748 <76087753+mikegarcia8748@users.noreply.github.com>
2022-02-26 14:37:20 +0800

    Testing redeemables add to cart

---
+++ g3appdriver/src/androidTest/java/org/rmj/g3appdriver/GCardCore/RedeemablesTransactionTest.java
@@ -0,0 +1,81 @@
+package org.rmj.g3appdriver.GCardCore;
+
+import android.content.Context;
+
+import static org.junit.Assert.assertTrue;
+
+import androidx.annotation.UiThread;
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.Observer;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.rmj.g3appdriver.dev.Database.Entities.ERedeemablesInfo;
+import org.rmj.g3appdriver.lib.GCardCore.GCardSystem;
+import org.rmj.g3appdriver.lib.GCardCore.Obj.CartItem;
+import org.rmj.g3appdriver.lib.GCardCore.iGCardSystem;
+
+import java.util.List;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(AndroidJUnit4.class)
+public class RedeemablesTransactionTest {
+
+
+    private Context mContext;
+    private iGCardSystem poSystem;
+
+    private boolean isSuccess = false;
+    private String message = "";
+
+    private static JSONObject poJson;
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Before
+    public void setUp() throws Exception {
+        mContext = ApplicationProvider.getApplicationContext();
+        poSystem = new GCardSystem(mContext).getInstance(GCardSystem.CoreFunctions.REDEEMPTION);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test @UiThread
+    public void test01AddToCart() throws Exception{
+        poSystem.GetRedeemablesList().observeForever(new Observer<List<ERedeemablesInfo>>() {
+            @Override
+            public void onChanged(List<ERedeemablesInfo> eRedeemablesInfos) {
+                ERedeemablesInfo loItem = eRedeemablesInfos.get(1);
+                CartItem loCart = new CartItem(loItem.getTransNox(),
+                        loItem.getPromoCde(),
+                        1,
+                        loItem.getPointsxx());
+
+                poSystem.AddToCart(loCart, new GCardSystem.GCardSystemCallback() {
+                    @Override
+                    public void OnSuccess(String args) {
+                        isSuccess = true;
+                    }
+
+                    @Override
+                    public void OnFailed(String message) {
+                        isSuccess = false;
+                    }
+                });
+            }
+        });
+        assertTrue(isSuccess);
+    }
+}
