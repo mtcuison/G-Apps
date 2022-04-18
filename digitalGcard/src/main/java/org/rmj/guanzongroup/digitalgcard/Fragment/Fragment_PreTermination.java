f81f4f018fb8129db048caf0891d43bec8b7c526
Kent <dkentpatrickd@gmail.com>
2022-04-18 13:35:03 +0800

    Added pre termination under gcard drawer

---
+++ digitalGcard/src/main/java/org/rmj/guanzongroup/digitalgcard/Fragment/Fragment_PreTermination.java
@@ -0,0 +1,30 @@
+package org.rmj.guanzongroup.digitalgcard.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.rmj.guanzongroup.digitalgcard.R;
+public class Fragment_PreTermination extends Fragment {
+
+    public Fragment_PreTermination() { }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pre_termination, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+}
