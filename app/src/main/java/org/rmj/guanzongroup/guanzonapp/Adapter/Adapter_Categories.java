49c10798956d9229763a48911cda4b163ffb81e2
Kent <dkentpatrickd@gmail.com>
2022-04-19 14:39:10 +0800

    Prepared category adapter for home screen

---
+++ app/src/main/java/org/rmj/guanzongroup/guanzonapp/Adapter/Adapter_Categories.java
@@ -0,0 +1,63 @@
+package org.rmj.guanzongroup.guanzonapp.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.rmj.guanzongroup.guanzonapp.R;
+
+public class Adapter_Categories extends RecyclerView.Adapter<Adapter_Categories.ViewHolderItem> {
+
+    private final OnItemClick poCallBck;
+
+    public Adapter_Categories(OnItemClick foCallBck){
+        this.poCallBck = foCallBck;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderItem onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View viewItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_categories, parent, false);
+        return new ViewHolderItem(viewItem, poCallBck);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolderItem holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolderItem extends RecyclerView.ViewHolder{
+
+        public ImageView imgCategx;
+        public TextView txtCategx;
+
+        public ViewHolderItem(@NonNull View itemView, OnItemClick foCallBck) {
+            super(itemView);
+            imgCategx = itemView.findViewById(R.id.img_category);
+            txtCategx = itemView.findViewById(R.id.txt_category);
+
+            itemView.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION) {
+                    foCallBck.onClick(position);
+                }
+            });
+        }
+
+    }
+
+    public interface OnItemClick {
+        void onClick(int position);
+    }
+
+}
