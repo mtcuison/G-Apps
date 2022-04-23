9e0bc267e81016868eded5b340e732700f777f68
Kent <dkentpatrickd@gmail.com>
2022-04-23 16:27:04 +0800

    Created Edit Personal Info,  Edit Account and Edit Address layout

--- useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Fragment/Fragment_EditAddress.java
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Fragment/Fragment_EditAddress.java
@@ -2,65 +2,39 @@ package org.rmj.guanzongroup.useraccount.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.rmj.guanzongroup.useraccount.R;
+import org.rmj.guanzongroup.useraccount.ViewModel.VMAccountDetails;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_EditAddress#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment_EditAddress extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private VMAccountDetails mViewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public Fragment_EditAddress() { }
 
-    public Fragment_EditAddress() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_EditAddress.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_EditAddress newInstance(String param1, String param2) {
-        Fragment_EditAddress fragment = new Fragment_EditAddress();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_edit_address, container, false);
+        initViews(view);
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(requireActivity()).get(VMAccountDetails.class);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_address, container, false);
+    // Initialize this first before anything else.
+    private void initViews(View view) {
+
     }
+
 }
