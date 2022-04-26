73dd52903a97b63f3f5d1d214fb903a1b09e145a
Kent <dkentpatrickd@gmail.com>
2022-04-26 13:53:28 +0800

    Prepare completion of account details UI

--- useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_CompleteAccountDetails.java
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_CompleteAccountDetails.java
@@ -7,7 +7,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.rmj.g3appdriver.etc.OnDateSetListener;
 import org.rmj.g3appdriver.utils.Dialogs.Dialog_SingleButton;
 import org.rmj.guanzongroup.useraccount.R;
 import org.rmj.guanzongroup.useraccount.ViewModel.VMAccountDetails;
@@ -18,6 +22,9 @@ public class Activity_CompleteAccountDetails extends AppCompatActivity {
 
     private VMAccountDetails mViewModel;
     private Toolbar toolbar;
+    private TextInputEditText txtLastNm, txtFirstN, txtMidNme, txtSuffix, txtBdatex, txtGender,
+            txtCivilS, txtTaxNox, txtHouseN, txtStreet;
+    private AutoCompleteTextView txtBplace, txtCtizen, txtTownCt, txtBarngy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,7 @@ public class Activity_CompleteAccountDetails extends AppCompatActivity {
                 .get(VMAccountDetails.class);
         initViews();
         setUpToolbar();
+        setInputOptions();
 
     }
 
@@ -46,6 +54,20 @@ public class Activity_CompleteAccountDetails extends AppCompatActivity {
     // Initialize this first before anything else.
     private void initViews() {
         toolbar = findViewById(R.id.toolbar);
+        txtLastNm = findViewById(R.id.tie_lastname);
+        txtFirstN = findViewById(R.id.tie_firstname);
+        txtMidNme = findViewById(R.id.tie_middname);
+        txtSuffix = findViewById(R.id.tie_suffix);
+        txtBdatex = findViewById(R.id.tie_bdate);
+        txtBplace = findViewById(R.id.tie_bplace);
+        txtGender = findViewById(R.id.tie_gender);
+        txtCivilS = findViewById(R.id.tie_civil_stat);
+        txtCtizen = findViewById(R.id.tie_citizen);
+        txtTaxNox = findViewById(R.id.tie_taxId);
+        txtHouseN = findViewById(R.id.tie_houseNo);
+        txtStreet = findViewById(R.id.tie_street);
+        txtTownCt = findViewById(R.id.tie_towncity);
+        txtBarngy = findViewById(R.id.tie_barangay);
     }
 
     // Initialize initViews() before this method.
@@ -54,4 +76,9 @@ public class Activity_CompleteAccountDetails extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Complete Details");
     }
+
+    private void setInputOptions() {
+        txtBdatex.addTextChangedListener(new OnDateSetListener(txtBdatex));
+    }
+
 }
