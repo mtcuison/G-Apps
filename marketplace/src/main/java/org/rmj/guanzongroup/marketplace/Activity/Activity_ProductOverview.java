f1170ffa73e072bb3902c5b8bc3906272526ab71
Kent <dkentpatrickd@gmail.com>
2022-05-11 11:42:00 +0800

    Redirect to Login when Adding to Cart/Clicking Buy now without logging in first.

--- marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_ProductOverview.java
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_ProductOverview.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -19,6 +20,7 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.rmj.g3appdriver.etc.CashFormatter;
+import org.rmj.g3appdriver.lib.Account.AccountInfo;
 import org.rmj.g3appdriver.utils.Dialogs.BottomDialog_AddToCart;
 import org.rmj.g3appdriver.utils.Dialogs.Dialog_Loading;
 import org.rmj.g3appdriver.utils.Dialogs.Dialog_SingleButton;
@@ -26,6 +28,7 @@ import org.rmj.guanzongroup.marketplace.Adapter.Adapter_ProductDescription;
 import org.rmj.guanzongroup.marketplace.Etc.OnTransactionsCallback;
 import org.rmj.guanzongroup.marketplace.R;
 import org.rmj.guanzongroup.marketplace.ViewModel.VMProductOverview;
+import org.rmj.guanzongroup.useraccount.Activity.Activity_Login;
 
 import java.util.Objects;
 
@@ -38,7 +41,7 @@ public class Activity_ProductOverview extends AppCompatActivity {
     private RecyclerView rvItmSpec;
     private TextView txtProdNm, txtUntPrc, txtSoldQt, txtBrandx, txtCatgry, txtColorx, txtStocks,
             txtBriefx;
-    private TextView btnAddCrt;
+    private TextView btnAddCrt, btnBuyNow;
 
     private String psItemIdx = "";
     private String psProduct = "";
@@ -56,6 +59,7 @@ public class Activity_ProductOverview extends AppCompatActivity {
         displayData();
 
         btnAddCrt.setOnClickListener(v -> addToCart());
+        btnBuyNow.setOnClickListener(v -> buyNow());
     }
 
     @Override
@@ -100,6 +104,7 @@ public class Activity_ProductOverview extends AppCompatActivity {
         txtBriefx = findViewById(R.id.txt_brief_desc);
 
         btnAddCrt = findViewById(R.id.btnText_addToCart);
+        btnBuyNow = findViewById(R.id.btnText_buyNow);
     }
 
     private void setUpToolbar() {
@@ -147,38 +152,60 @@ public class Activity_ProductOverview extends AppCompatActivity {
     }
 
     private void addToCart() {
-        final BottomDialog_AddToCart dialog = new BottomDialog_AddToCart(psProduct, psPricexx, fnItemQty -> {
-            try {
-                mViewModel.addUpdateCart(psItemIdx, fnItemQty, new OnTransactionsCallback() {
-                    @Override
-                    public void onLoading() {
-                        poLoading = new Dialog_Loading(Activity_ProductOverview.this);
-                        poLoading.initDialog("Add to Cart", "Adding to cart. Please wait.");
-                        poLoading.show();
-                    }
+        if(isLoggedIn()) {
+            final BottomDialog_AddToCart dialog = new BottomDialog_AddToCart(psProduct, psPricexx,
+                    fnItemQty -> {
+                try {
+                    mViewModel.addUpdateCart(psItemIdx, fnItemQty, new OnTransactionsCallback() {
+                        @Override
+                        public void onLoading() {
+                            poLoading = new Dialog_Loading(Activity_ProductOverview.this);
+                            poLoading.initDialog("Add to Cart",
+                                    "Adding to cart. Please wait.");
+                            poLoading.show();
+                        }
 
-                    @Override
-                    public void onSuccess(String fsMessage) {
-                        poLoading.dismiss();
-                        poDialogx.setButtonText("Okay");
-                        poDialogx.initDialog("Add to Cart", "Successfully added to cart.",
-                                dialog -> dialog.dismiss());
-                        poDialogx.show();
-                    }
+                        @Override
+                        public void onSuccess(String fsMessage) {
+                            poLoading.dismiss();
+                            poDialogx.setButtonText("Okay");
+                            poDialogx.initDialog("Add to Cart",
+                                    "Successfully added to cart.",
+                                    dialog -> dialog.dismiss());
+                            poDialogx.show();
+                        }
 
-                    @Override
-                    public void onFailed(String fsMessage) {
-                        poLoading.dismiss();
-                        poDialogx.setButtonText("Okay");
-                        poDialogx.initDialog("Add to Cart", fsMessage, dialog -> dialog.dismiss());
-                        poDialogx.show();
-                    }
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        dialog.show(getSupportFragmentManager(),"Add To Cart");
+                        @Override
+                        public void onFailed(String fsMessage) {
+                            poLoading.dismiss();
+                            poDialogx.setButtonText("Okay");
+                            poDialogx.initDialog("Add to Cart", fsMessage,
+                                    dialog -> dialog.dismiss());
+                            poDialogx.show();
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            dialog.show(getSupportFragmentManager(),"Add To Cart");
+        }
+    }
+
+    private void buyNow() {
+        if(isLoggedIn()) {
+
+        }
+    }
+
+    private boolean isLoggedIn() {
+        if(!new AccountInfo(Activity_ProductOverview.this).getLoginStatus()) {
+            Intent loIntent = new Intent(Activity_ProductOverview.this, Activity_Login.class);
+            startActivity(loIntent);
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
