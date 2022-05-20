43c97ed2aba69e48ee6597a11aebf6697f710d0e
Kent <dkentpatrickd@gmail.com>
2022-05-20 11:48:32 +0800

    Prepared pay order functionality

--- marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_PayOrder.java
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_PayOrder.java
@@ -13,7 +13,12 @@ import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
+import org.rmj.g3appdriver.etc.PaymentMethod;
 import org.rmj.g3appdriver.utils.Dialogs.Dialog_DoubleButton;
+import org.rmj.g3appdriver.utils.Dialogs.Dialog_Loading;
+import org.rmj.g3appdriver.utils.Dialogs.Dialog_SingleButton;
+import org.rmj.g3appdriver.utils.Dialogs.Dialog_TextInput;
+import org.rmj.guanzongroup.marketplace.Etc.OnTransactionsCallback;
 import org.rmj.guanzongroup.marketplace.R;
 import org.rmj.guanzongroup.marketplace.ViewModel.VMPayOrder;
 
@@ -23,8 +28,12 @@ public class Activity_PayOrder extends AppCompatActivity {
 
     private VMPayOrder mViewModel;
     private Toolbar toolBar;
+    private Dialog_SingleButton poDialogx;
+    private Dialog_Loading poLoading;
     private RecyclerView recyclerView;
     private MaterialButton btnPayOrd;
+
+    private PaymentMethod poPayMeth;
     private String psTransNo = "";
 
     @Override
@@ -37,7 +46,9 @@ public class Activity_PayOrder extends AppCompatActivity {
         setUpToolbar();
         displayPaymentOptions();
 
-        btnPayOrd.setOnClickListener(v -> {});
+        btnPayOrd.setOnClickListener(v -> {
+            if(isMethodSelected()) { payOrder(); }
+        });
     }
 
     @Override
@@ -65,6 +76,7 @@ public class Activity_PayOrder extends AppCompatActivity {
 
     private void initViews() {
         toolBar = findViewById(R.id.toolbar);
+        poDialogx = new Dialog_SingleButton(Activity_PayOrder.this);
         recyclerView = findViewById(R.id.recyclerView);
         btnPayOrd = findViewById(R.id.btnPayOrder);
     }
@@ -77,6 +89,72 @@ public class Activity_PayOrder extends AppCompatActivity {
 
     private void displayPaymentOptions() {
 
+    }
+
+    private void payOrder() {
+        final Dialog_TextInput loDialog = new Dialog_TextInput(Activity_PayOrder.this);
+        loDialog.initDialog("Reference Number", new Dialog_TextInput.OnDialogConfirmation() {
+            @Override
+            public void onConfirm(String fsInputx, AlertDialog dialog) {
+                if(!fsInputx.isEmpty()) {
+                    dialog.dismiss();
+                    mViewModel.payOrder(psTransNo, poPayMeth, fsInputx, new OnTransactionsCallback() {
+                        @Override
+                        public void onLoading() {
+                            poLoading = new Dialog_Loading(Activity_PayOrder.this);
+                            poLoading.initDialog("Pay Order", "Payment Processing. Please wait.");
+                            poLoading.show();
+                        }
+
+                        @Override
+                        public void onSuccess(String fsMessage) {
+                            poLoading.dismiss();
+                            poDialogx.setButtonText("Okay");
+                            poDialogx.initDialog("Pay Order", fsMessage, dialog1 -> {
+                                dialog1.dismiss();
+                                finish();
+                            });
+                            poDialogx.show();
+                        }
+
+                        @Override
+                        public void onFailed(String fsMessage) {
+                            poLoading.dismiss();
+                            poDialogx.setButtonText("Okay");
+                            poDialogx.initDialog("Pay Order", fsMessage, dialog1 -> dialog1.dismiss());
+                            poDialogx.show();
+                        }
+                    });
+                } else {
+                    dialog.dismiss();
+                    poDialogx.setButtonText("Okay");
+                    poDialogx.initDialog("Pay Order",
+                            "Please enter payment reference number.", dialog1 -> {
+                        dialog1.dismiss();
+                    });
+                    poDialogx.show();
+                }
+            }
+
+            @Override
+            public void onCancel(AlertDialog dialog) {
+                dialog.dismiss();
+            }
+        });
+        loDialog.show();
+    }
+
+    private boolean isMethodSelected() {
+        if(poPayMeth == null) {
+            poDialogx.setButtonText("Okay");
+            poDialogx.initDialog("Pay Order",
+                    "Please select payment method for your order.", dialog -> {
+                        dialog.dismiss();
+                    });
+            poDialogx.show();
+            return false;
+        }
+        return true;
     }
 
     private void popUpCloseConfirmationDialog() {
