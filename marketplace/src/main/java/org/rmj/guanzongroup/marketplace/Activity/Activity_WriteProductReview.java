90e1425b01b7c6854cb24c7eda17aaadc9eeda80
Kent <dkentpatrickd@gmail.com>
2022-05-26 09:52:13 +0800

    Initialized viewModel to activity write product and add back feature functions.

--- marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_WriteProductReview.java
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_WriteProductReview.java
@@ -1,19 +1,60 @@
 package org.rmj.guanzongroup.marketplace.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import org.rmj.g3appdriver.utils.Dialogs.Dialog_DoubleButton;
+import org.rmj.guanzongroup.marketplace.ViewModel.VMWriteProductReview;
 import org.rmj.guanzongroup.marketplace.databinding.ActivityWriteProductReviewBinding;
 
 public class Activity_WriteProductReview extends AppCompatActivity {
 
-    private ActivityWriteProductReviewBinding binding;
+    private VMWriteProductReview mViewModel;
+    private ActivityWriteProductReviewBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityWriteProductReviewBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mViewModel = new ViewModelProvider(Activity_WriteProductReview.this)
+                .get(VMWriteProductReview.class);
+        mBinding = ActivityWriteProductReviewBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            popUpCloseConfirmationDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        popUpCloseConfirmationDialog();
+    }
+
+    private void popUpCloseConfirmationDialog() {
+        Dialog_DoubleButton loDblDiag = new Dialog_DoubleButton(Activity_WriteProductReview.this);
+        loDblDiag.setButtonText("Yes", "No");
+        loDblDiag.initDialog("Product Review", "Are you sure you want to cancel reviewing your product?", new Dialog_DoubleButton.OnDialogConfirmation() {
+            @Override
+            public void onConfirm(AlertDialog dialog) {
+                dialog.dismiss();
+                finish();
+            }
+
+            @Override
+            public void onCancel(AlertDialog dialog) {
+                dialog.dismiss();
+            }
+        });
+        loDblDiag.show();
+    }
+
 }
---
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/ViewModel/VMWriteProductReview.java
@@ -0,0 +1,15 @@
+package org.rmj.guanzongroup.marketplace.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class VMWriteProductReview extends AndroidViewModel {
+
+    public VMWriteProductReview(@NonNull Application application) {
+        super(application);
+    }
+
+
+}
