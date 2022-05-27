9f0341d1da671c840db577a8002e68c54d094cce
Kent <dkentpatrickd@gmail.com>
2022-05-27 17:07:51 +0800

    Created initial files for Shipping Address modification.

---
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Fragment/Fragment_AddressList.java
@@ -0,0 +1,42 @@
+package org.rmj.guanzongroup.useraccount.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.rmj.guanzongroup.useraccount.ViewModel.VMShippingAddress;
+import org.rmj.guanzongroup.useraccount.databinding.FragmentAddressListBinding;
+
+public class Fragment_AddressList extends Fragment {
+
+    private VMShippingAddress mViewModel;
+    private FragmentAddressListBinding mBinding;
+
+    public Fragment_AddressList() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mBinding = FragmentAddressListBinding.inflate(inflater, container, false);
+        View view = mBinding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(requireActivity()).get(VMShippingAddress.class);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+}
