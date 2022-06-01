563419a37934a7b42c39a33d2b7a6d093291fa98
Kent <dkentpatrickd@gmail.com>
2022-06-01 15:00:52 +0800

    Set default value in editing personal data

--- useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Fragment/Fragment_EditPersonalInfo.java
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Fragment/Fragment_EditPersonalInfo.java
@@ -10,31 +10,94 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.rmj.guanzongroup.useraccount.R;
+import org.rmj.g3appdriver.etc.InputFieldController;
 import org.rmj.guanzongroup.useraccount.ViewModel.VMAccountDetails;
+import org.rmj.guanzongroup.useraccount.databinding.FragmentEditPersonalInfoBinding;
+
+import java.util.ArrayList;
 
 public class Fragment_EditPersonalInfo extends Fragment {
 
+    private FragmentEditPersonalInfoBinding mBinding;
     private VMAccountDetails mViewModel;
 
     public Fragment_EditPersonalInfo() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_edit_personal_info, container, false);
-        initViews(view);
-        return view;
+        mBinding =  FragmentEditPersonalInfoBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(requireActivity()).get(VMAccountDetails.class);
+        initSelector();
+        setDefaultValues();
     }
 
-    // Initialize this first before anything else.
-    private void initViews(View view) {
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
     }
+
+    private void initSelector() {
+        mBinding.txtGender.setAdapter(
+                InputFieldController.getAutoCompleteData(
+                        requireActivity(), mViewModel.getGenderList()
+                ));
+
+        mBinding.txtCivilS.setAdapter(
+                InputFieldController.getAutoCompleteData(
+                        requireActivity(), mViewModel.getCivilStatusList()
+                ));
+
+        mViewModel.getCountryList().observe(getViewLifecycleOwner(), countries -> {
+            try {
+                ArrayList<String> lsCountry = mViewModel.getCountryForInput(countries);
+                mBinding.txtCtizen.setAdapter(
+                        InputFieldController.getAutoCompleteData(
+                                requireActivity(),
+                                lsCountry
+                        )
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        mBinding.txtGender.setOnItemClickListener((adapterView, view, i, l) -> {});
+    }
+
+    private void setDefaultValues() {
+        mViewModel.getClientInfo().observe(getViewLifecycleOwner(), eClientInfo -> {
+            try {
+                mBinding.txtGender.setText(mViewModel.getGenderList()
+                        .get(Integer.parseInt(eClientInfo.getGenderCd())));
+                mBinding.txtCivilS.setText(mViewModel.getCivilStatusList()
+                        .get(Integer.parseInt(eClientInfo.getCvilStat())));
+                mViewModel.getCountryList().observe(getViewLifecycleOwner(), eCountryInfos -> {
+                    try {
+                         for(int x = 0; x < eCountryInfos.size(); x++) {
+                             if(eClientInfo.getCitizenx().equalsIgnoreCase(eCountryInfos.get(x).getCntryCde())) {
+                                 mBinding.txtCtizen.setText(eCountryInfos.get(x).getNational());
+                                 break;
+                             } else {
+                                 continue;
+                             }
+                         }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                mBinding.txtTaxIdN.setText(eClientInfo.getTaxIDNox());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
 
 }
