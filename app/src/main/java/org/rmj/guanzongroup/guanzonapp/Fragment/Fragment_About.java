83831b266e27bb5861aaa36fa5fe8253185ff64f
Kent <dkentpatrickd@gmail.com>
2022-06-04 09:49:57 +0800

    Added terms and conditions and privacy policy link in about fragment.

--- app/src/main/java/org/rmj/guanzongroup/guanzonapp/Fragment/Fragment_About.java
+++ app/src/main/java/org/rmj/guanzongroup/guanzonapp/Fragment/Fragment_About.java
@@ -1,5 +1,6 @@
 package org.rmj.guanzongroup.guanzonapp.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,11 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import org.rmj.guanzongroup.guanzonapp.R;
+
+import org.rmj.guanzongroup.guanzonapp.Activity.Activity_TermsAndConditions;
 import org.rmj.guanzongroup.guanzonapp.databinding.FragmentAboutBinding;
 
-public class Fragment_About extends Fragment {;
+public class Fragment_About extends Fragment {
 
     private FragmentAboutBinding mBinding;
 
@@ -20,6 +22,20 @@ public class Fragment_About extends Fragment {;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBinding = FragmentAboutBinding.inflate(inflater, container, false);
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Intent loIntent = new Intent(requireActivity(), Activity_TermsAndConditions.class);
+        mBinding.lnkTermsC.setOnClickListener(v -> {
+            loIntent.putExtra("cTermsDsp", 0);
+            startActivity(loIntent);
+        });
+        mBinding.lnkPrivcy.setOnClickListener(v -> {
+            loIntent.putExtra("cTermsDsp", 1);
+            startActivity(loIntent);
+        });
     }
 
     @Override
