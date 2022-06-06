7cf41e7084268531c8226b2e95c2805d30ad5819
Kent <dkentpatrickd@gmail.com>
2022-06-06 09:49:16 +0800

    Add image setter in adapter categories card view background.

--- marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Adapter/Adapter_Categories.java
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Adapter/Adapter_Categories.java
@@ -1,5 +1,9 @@
 package org.rmj.guanzongroup.marketplace.Adapter;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.rmj.guanzongroup.marketplace.R;
@@ -14,9 +19,11 @@ import org.rmj.guanzongroup.marketplace.R;
 
 public class Adapter_Categories extends RecyclerView.Adapter<Adapter_Categories.ViewHolderItem> {
 
+    private final Context poContext;
     private final OnItemClick poCallBck;
 
-    public Adapter_Categories(OnItemClick foCallBck){
+    public Adapter_Categories(Context foContext, OnItemClick foCallBck){
+        this.poContext = foContext;
         this.poCallBck = foCallBck;
     }
 
@@ -29,7 +36,13 @@ public class Adapter_Categories extends RecyclerView.Adapter<Adapter_Categories.
 
     @Override
     public void onBindViewHolder(ViewHolderItem holder, int position) {
-
+        try {
+//            Bitmap loImageBg = BitmapFactory.decodeStream(is); // Put Image URL here.
+//            BitmapDrawable background = new BitmapDrawable(poContext.getResources(), loImageBg);
+//            holder.lnCategry.setBackground(background);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -37,22 +50,25 @@ public class Adapter_Categories extends RecyclerView.Adapter<Adapter_Categories.
         return 0;
     }
 
-    public static class ViewHolderItem extends RecyclerView.ViewHolder{
+    public static class ViewHolderItem extends RecyclerView.ViewHolder {
 
+        public ConstraintLayout lnCategry;
         public ImageView imgCategx;
         public TextView txtCategx;
 
         public ViewHolderItem(@NonNull View itemView, OnItemClick foCallBck) {
             super(itemView);
+            lnCategry = itemView.findViewById(R.id.lnCategory);
             imgCategx = itemView.findViewById(R.id.img_category);
             txtCategx = itemView.findViewById(R.id.txt_category);
 
-            itemView.setOnClickListener(v -> {
+            lnCategry.setOnClickListener(v -> {
                 int position = getAdapterPosition();
                 if(position != RecyclerView.NO_POSITION) {
                     foCallBck.onClick(position);
                 }
             });
+
         }
 
     }
