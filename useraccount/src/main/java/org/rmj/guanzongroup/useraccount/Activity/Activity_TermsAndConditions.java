94f942b45f68b148fdf0f08f8157c5faf10e3a1b
Kent <dkentpatrickd@gmail.com>
2022-06-13 16:47:35 +0800

    Added terms and conditions checker in sign up activity.

---
+++ useraccount/src/main/java/org/rmj/guanzongroup/useraccount/Activity/Activity_TermsAndConditions.java
@@ -0,0 +1,39 @@
+package org.rmj.guanzongroup.useraccount.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.rmj.guanzongroup.useraccount.R;
+
+
+public class Activity_TermsAndConditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_conditions);
+        TextView lblTermsAndConditions = findViewById(R.id.lbl_termsAndConditions);
+
+        if(getIntent().hasExtra("cTermsDsp")){
+            int cTermsDsp = getIntent().getIntExtra("cTermsDsp", 0);
+            if(cTermsDsp == 0){
+                lblTermsAndConditions.setText(Html.fromHtml(getString(R.string.Terms_and_Conditions)));
+            } else {
+                lblTermsAndConditions.setText(Html.fromHtml(getString(R.string.Privacy_Policy)));
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent loIntent = new Intent();
+        loIntent.putExtra("result", 0);
+        setResult(Activity.RESULT_OK, loIntent);
+        finish();
+    }
+}
