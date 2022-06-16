16c2d1cc2bf96b1a3519a6455fd99ac39083260d
Kent <dkentpatrickd@gmail.com>
2022-06-16 16:02:58 +0800

    Show all product FAQs list.

--- marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_ProductQueries.java
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_ProductQueries.java
@@ -2,17 +2,30 @@ package org.rmj.guanzongroup.marketplace.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.rmj.g3appdriver.etc.CashFormatter;
 import org.rmj.g3appdriver.utils.Dialogs.Dialog_SingleButton;
+import org.rmj.guanzongroup.marketplace.Adapter.Adapter_ProductQueries;
+import org.rmj.guanzongroup.marketplace.ViewModel.VMProductQueries;
 import org.rmj.guanzongroup.marketplace.databinding.ActivityProductQueriesBinding;
 
 import java.util.Objects;
 
 public class Activity_ProductQueries extends AppCompatActivity {
-
+    private final String TAG = Activity_ProductQueries.class.getSimpleName();
+    private VMProductQueries mViewModel;
     private ActivityProductQueriesBinding mBinding;
     private Dialog_SingleButton poDialogx;
     private String psItemIdx = "";
@@ -22,6 +35,7 @@ public class Activity_ProductQueries extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mBinding = ActivityProductQueriesBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+        mViewModel = new ViewModelProvider(Activity_ProductQueries.this).get(VMProductQueries.class);
         getExtras();
 
         setSupportActionBar(mBinding.toolbar);
@@ -29,6 +43,8 @@ public class Activity_ProductQueries extends AppCompatActivity {
         getSupportActionBar().setTitle("Asked Questions");
 
         poDialogx = new Dialog_SingleButton(Activity_ProductQueries.this);
+
+        displayData();
     }
 
     @Override
@@ -55,6 +71,73 @@ public class Activity_ProductQueries extends AppCompatActivity {
             });
             poDialogx.show();
         }
+    }
+
+    private void displayData() {
+        showProductInfo();
+        showInquiries();
+    }
+
+    private void showProductInfo() {
+        mViewModel.getProductInfo(psItemIdx).observe(Activity_ProductQueries.this, product -> {
+            try {
+                String lsSoldQty = "Sold: " + product.getSoldQtyx();
+                String sampleImg = "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MHKD3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1623348270000";
+                Picasso.get().load(sampleImg).into(mBinding.imgProdct);
+                mBinding.txtProdNm.setText(product.getModelNme());
+                mBinding.txtSoldQt.setText(lsSoldQty);
+                mBinding.txtPricex.setText(CashFormatter.parse(product.getUnitPrce()));
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                finish();
+            }
+        });
+    }
+
+    private void showInquiries() {
+        mViewModel.ImportInquiries(psItemIdx, new VMProductQueries.OnInquiryReviewsImportCallback() {
+            @Override
+            public void OnImport(String args) {
+                try {
+                    JSONObject loJson = new JSONObject(args);
+                    mBinding.rvQueries.setVisibility(View.VISIBLE);
+                    mBinding.lblNoFaqs.setVisibility(View.GONE);
+                    Adapter_ProductQueries loAdapter = new Adapter_ProductQueries(
+                            getFilteredFaqs(loJson.getJSONArray("detail")),
+                            false);
+                    loAdapter.notifyDataSetChanged();
+                    mBinding.rvQueries.setLayoutManager(new LinearLayoutManager(Activity_ProductQueries.this));
+                    mBinding.rvQueries.setHasFixedSize(true);
+                    mBinding.rvQueries.setAdapter(loAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    mBinding.rvQueries.setVisibility(View.GONE);
+                    mBinding.lblNoFaqs.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void OnFailed(String message) {
+                Log.e(TAG, message);
+                mBinding.rvQueries.setVisibility(View.GONE);
+                mBinding.lblNoFaqs.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private JSONArray getFilteredFaqs(JSONArray foArray) {
+        JSONArray loArray = new JSONArray();
+        for(int x = 0; x < foArray.length(); x++) {
+            try {
+                if (!foArray.getJSONObject(x).getString("sReplyxxx")
+                        .equalsIgnoreCase("null")) {
+                    loArray.put(foArray.getJSONObject(x));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return loArray;
     }
 
 }
---
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/ViewModel/VMProductQueries.java
@@ -0,0 +1,80 @@
+package org.rmj.guanzongroup.marketplace.ViewModel;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import org.json.JSONObject;
+import org.rmj.g3appdriver.dev.Database.Entities.EProducts;
+import org.rmj.g3appdriver.dev.Repositories.RProduct;
+import org.rmj.g3appdriver.etc.ConnectionUtil;
+
+public class VMProductQueries extends AndroidViewModel {
+
+    private final Application application;
+    private final RProduct poProduct;
+
+    public VMProductQueries(@NonNull Application application) {
+        super(application);
+        this.application = application;
+        this.poProduct = new RProduct(application);
+    }
+
+    public LiveData<EProducts> getProductInfo(String fsListID){
+        return poProduct.GetProductInfo(fsListID);
+    }
+
+    public void ImportInquiries(String fsVal, OnInquiryReviewsImportCallback callback){
+        new ImportInquiriesTask(callback).execute(fsVal);
+    }
+
+    private class ImportInquiriesTask extends AsyncTask<String, Void, String> {
+
+        private OnInquiryReviewsImportCallback callback;
+        private final ConnectionUtil loConnect;
+        private boolean isSuccess = false;
+        private String message;
+        private JSONObject poData;
+
+        public ImportInquiriesTask(OnInquiryReviewsImportCallback callback) {
+            this.callback = callback;
+            this.loConnect = new ConnectionUtil(application);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            if(loConnect.isDeviceConnected()) {
+                if (poProduct.GetQuestionsAndAnswers(strings[0])) {
+                    poData = poProduct.getData();
+                    isSuccess = true;
+                } else {
+                    message = poProduct.getMessage();
+                    isSuccess = false;
+                }
+            } else {
+                message = "No internet";
+                isSuccess = false;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(isSuccess){
+                callback.OnImport(poData.toString());
+            } else {
+                callback.OnFailed(message);
+            }
+        }
+    }
+
+    public interface OnInquiryReviewsImportCallback{
+        void OnImport(String args);
+        void OnFailed(String message);
+    }
+
+}
