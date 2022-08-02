af4d8477cd5787615507e90ee5a574c9335dbff1
mikegarcia8748 <76087753+mikegarcia8748@users.noreply.github.com>
2022-08-02 17:25:16 +0800

    Adjusted the parameters pass through intent for product overview.
    Creating new UI for preview of regular messages
    
    Added new field for Sales Order Master

---
+++ marketplace/src/main/java/org/rmj/guanzongroup/marketplace/Activity/Activity_LoanApplication.java
@@ -0,0 +1,16 @@
+package org.rmj.guanzongroup.marketplace.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.rmj.guanzongroup.marketplace.R;
+
+public class Activity_LoanApplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_application);
+    }
+}
