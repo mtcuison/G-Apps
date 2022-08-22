c6b0e50b43997b441ca7f643d593c01e9f43e8dd
Kent <dkentpatrickd@gmail.com>
2022-08-22 17:21:16 +0800

    CreditAPP UI

---
+++ creditApp/src/main/java/org/guanzongroup/com/creditapp/Activities/Activity_OtherInfo.java
@@ -0,0 +1,16 @@
+package org.guanzongroup.com.creditapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.guanzongroup.com.creditapp.R;
+
+public class Activity_OtherInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_info);
+    }
+}
