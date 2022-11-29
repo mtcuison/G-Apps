fd97d01ba4f054fb0ea6dcf68d0fa3c0bd8f8a2f
mikegarcia <mikegarcia8748@gmail.com>
2022-11-29 13:25:47 +0800

    Preparation for Guanzon_Panalo Project

---
+++ notifications/src/main/java/org/rmj/guanzongroup/notifications/Obj/Notification.java
@@ -0,0 +1,13 @@
+package org.rmj.guanzongroup.notifications.Obj;
+
+import android.content.Context;
+
+public class Notification {
+    private static final String TAG = Notification.class.getSimpleName();
+
+    public Notification(Context context){
+
+    }
+
+    
+}
