c29b3958ff435a268049617cc6adf2e8feade8ab
mikegarcia <mikegarcia8748@gmail.com>
2022-12-02 14:51:27 +0800

    Created UI classes for guanzon panalo project

---
+++ guanzonPanalo/src/main/java/org/rmj/guanzongroup/panalo/Dialog/DialogPanaloClaim.java
@@ -0,0 +1,6 @@
+package org.rmj.guanzongroup.panalo.Dialog;
+
+public class DialogPanaloClaim {
+
+    //TODO: This dialog will be use for claiming the reward
+}
---
+++ guanzonPanalo/src/main/java/org/rmj/guanzongroup/panalo/ViewModel/VMPanaloDashboard.java
@@ -0,0 +1,7 @@
+package org.rmj.guanzongroup.panalo.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class VMPanaloDashboard extends ViewModel {
+    // TODO: Implement the ViewModel
+}
