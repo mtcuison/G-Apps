31b910b8223589f7bcebbd75f1e0e7ba2dc4069f
Jeitee <iamjeitee.8@gmail.com>
2023-09-05 13:56:40 +0800

    9 4 2023 call fragm,ent brand selection on drawer

--- ganado/src/main/java/org/rmj/guanzongroup/ganado/Fragment/Fragment_Brand_Selection.java
+++ ganado/src/main/java/org/rmj/guanzongroup/ganado/Fragment/Fragment_Brand_Selection.java
@@ -28,7 +28,7 @@ import org.rmj.guanzongroup.ganado.R;
 import org.rmj.guanzongroup.ganado.ViewModel.VMBrandList;
 import org.rmj.guanzongroup.ganado.ViewModel.VMBrandSelection;
 
-public class Fragment_Brand_Selection extends AppCompatActivity {
+public class Fragment_Brand_Selection extends Fragment {
     private RecyclerView rvBrand;
     private VMBrandSelection mViewModel;
     private RecyclerViewAdapter_BrandSelection rec_brandList;
@@ -38,44 +38,48 @@ public class Fragment_Brand_Selection extends AppCompatActivity {
         return new Fragment_Brand_Selection();
     }
 
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-//                             @Nullable Bundle savedInstanceState) {
-//        mViewModel = new ViewModelProvider(requireActivity()).get(VMBrandSelection.class);
-//        View view = inflater.inflate(R.layout.fragment_brand_selection, container, false);
-//        initViews(view);
-//        displayData();
-//        return view;
-//    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_brand_selection);
-//        initViews();
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mViewModel = new ViewModelProvider(requireActivity()).get(VMBrandSelection.class);
+        View view = inflater.inflate(R.layout.fragment_brand_selection, container, false);
         mViewModel = new ViewModelProvider(this).get(VMBrandSelection.class);
 
-        brandcatimg = findViewById(R.id.imagebrandtop);
+        brandcatimg = view.findViewById(R.id.imagebrandtop);
 
-        mViewModel.getBrandList().observe(Fragment_Brand_Selection.this, brandList -> {
-            if (brandList.size() > 0) {
-                rec_brandList = new RecyclerViewAdapter_BrandSelection(brandList, new RecyclerViewAdapter_BrandSelection.OnBrandSelectListener() {
-                    @Override
-                    public void OnSelect(String BrandID, String BrandName) {
-                        Intent intent = new Intent(Fragment_Brand_Selection.this, Activity_Brand_List.class);
-                        intent.putExtra("lsBrandID", BrandID);
-                        intent.putExtra("lsBrandNm", BrandName);
-                        startActivity(intent);
-                        overridePendingTransition(R.anim.anim_intent_slide_in_right, R.anim.anim_intent_slide_out_left);
-                    }
-                });
+        mViewModel.getBrandList().observe(getViewLifecycleOwner(), s -> {
+            try {
+                if (s.size() > 0) {
+                    rec_brandList = new RecyclerViewAdapter_BrandSelection(s, new RecyclerViewAdapter_BrandSelection.OnBrandSelectListener() {
+                        @Override
+                        public void OnSelect(String BrandID, String BrandName) {
+                            Intent intent = new Intent(requireActivity(), Activity_Brand_List.class);
+                            intent.putExtra("lsBrandID", BrandID);
+                            intent.putExtra("lsBrandNm", BrandName);
+                            startActivity(intent);
+                        }
+                    });
 
-                rvBrand.setAdapter(rec_brandList);
-                rvBrand.setLayoutManager(new GridLayoutManager(Fragment_Brand_Selection.this, 2, RecyclerView.VERTICAL, false));
+                    rvBrand.setAdapter(rec_brandList);
+                    rvBrand.setLayoutManager(new GridLayoutManager(requireActivity(), 2, RecyclerView.VERTICAL, false));
 
-                rec_brandList.notifyDataSetChanged();
+                    rec_brandList.notifyDataSetChanged();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
             }
         });
+
+
+
+        return view;
     }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_brand_selection);
+////        initViews();
+//
+//    }
 
 }
